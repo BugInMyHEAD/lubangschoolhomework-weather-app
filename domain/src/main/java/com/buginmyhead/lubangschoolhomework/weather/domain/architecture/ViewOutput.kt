3670c635a5ec816b8430fbfc1ce742bdaf1ae6a1
Mintcho GU <gokim1@coupang.com>
2022-10-10 01:54:18 +0900

    Change type

--- domain/src/main/java/com/buginmyhead/lubangschoolhomework/weather/domain/architecture/ViewOutput.kt
+++ domain/src/main/java/com/buginmyhead/lubangschoolhomework/weather/domain/architecture/ViewOutput.kt
@@ -1,11 +1,10 @@
 package com.buginmyhead.lubangschoolhomework.weather.domain.architecture
 
 import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.ObservableSource
 
 interface ViewOutput<S, L, F> {
 
-    val state: ObservableSource<ViewData<S, L, F>>
+    val state: Observable<ViewData<S, L, F>>
 
 }
 
