6c97af590878e6b05bf4da15ade9a094e9101aae
Mintcho GU <gokim1@coupang.com>
2022-10-23 20:31:12 +0900

    Divide WeatherInfoModule.kt

---
+++ app/src/main/java/com/buginmyhead/lubangschoolhomework/weather/app/main/di/MainUiModule.kt
@@ -0,0 +1,27 @@
+package com.buginmyhead.lubangschoolhomework.weather.app.main.di
+
+import com.buginmyhead.lubangschoolhomework.weather.architecture.ViewController
+import com.buginmyhead.lubangschoolhomework.weather.architecture.ViewState
+import com.buginmyhead.lubangschoolhomework.weather.domain.weatherinfo.ThreeDayWeatherInfo
+import com.buginmyhead.lubangschoolhomework.weather.domain.weatherinfo.WeatherInfoFailure
+import com.buginmyhead.lubangschoolhomework.weather.domain.weatherinfo.WeatherInfoLoading
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+import io.reactivex.rxjava3.core.Observable
+
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object WeatherInfoModuleViewModelProvider {
+
+    @Provides
+    @ViewModelScoped
+    fun provideWeatherInfoOutput(
+        viewController: ViewController<ThreeDayWeatherInfo, WeatherInfoLoading, WeatherInfoFailure>
+    ): Observable<ViewState<ThreeDayWeatherInfo, WeatherInfoLoading, WeatherInfoFailure>> =
+        viewController.output
+
+}
--- app/src/main/java/com/buginmyhead/lubangschoolhomework/weather/data/weatherinfo/di/WeatherInfoModule.kt
+++ app/src/main/java/com/buginmyhead/lubangschoolhomework/weather/data/weatherinfo/di/WeatherInfoModule.kt
@@ -1,11 +1,8 @@
-package com.buginmyhead.lubangschoolhomework.weather.app.di
+package com.buginmyhead.lubangschoolhomework.weather.data.weatherinfo.di
 
 import com.buginmyhead.lubangschoolhomework.weather.architecture.ReadOnlyRepository
 import com.buginmyhead.lubangschoolhomework.weather.architecture.ViewController
-import com.buginmyhead.lubangschoolhomework.weather.architecture.ViewState
 import com.buginmyhead.lubangschoolhomework.weather.data.weatherinfo.OpenMeteo
-import com.buginmyhead.lubangschoolhomework.weather.data.weatherinfo.WeatherInfoRemoteDataSource
-import com.buginmyhead.lubangschoolhomework.weather.data.weatherinfo.WeatherInfoRemoteDataSourceDummyImpl
 import com.buginmyhead.lubangschoolhomework.weather.data.weatherinfo.ThreeDayWeatherInfoRepositoryImpl
 import com.buginmyhead.lubangschoolhomework.weather.domain.weatherinfo.RefreshWeatherInfoUseCase
 import com.buginmyhead.lubangschoolhomework.weather.domain.weatherinfo.RefreshWeatherInfoUseCaseImpl
@@ -17,10 +14,7 @@ import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
-import dagger.hilt.android.scopes.ViewModelScoped
 import dagger.hilt.components.SingletonComponent
-import io.reactivex.rxjava3.core.Observable
 import javax.inject.Singleton
 
 @Module
@@ -54,18 +48,5 @@ object WeatherInfoModuleProvider {
     @Provides
     @Singleton
     fun provideOpenMeteoRemoteDataSource(): OpenMeteo.RemoteDataSource = OpenMeteo.RemoteDataSourceImpl
-
-}
-
-@Module
-@InstallIn(ViewModelComponent::class)
-object WeatherInfoModuleViewModelProvider {
-
-    @Provides
-    @ViewModelScoped
-    fun provideWeatherInfoOutput(
-        viewController: ViewController<ThreeDayWeatherInfo, WeatherInfoLoading, WeatherInfoFailure>
-    ): Observable<ViewState<ThreeDayWeatherInfo, WeatherInfoLoading, WeatherInfoFailure>> =
-        viewController.output
 
 }
